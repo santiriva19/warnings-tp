143605ce26564209b3be7e4d5380e1f89be37dff
Santiago <ttsbpn@gmail.com>
2020-11-29 22:45:21 -0500

    finish front on alerts

--- src/Components/dashboard/TypeOfAlerts/TypeOfAlerts.js
+++ src/Components/dashboard/TypeOfAlerts/TypeOfAlerts.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Component } from 'react';
 import './TypeOfAlerts.css'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
-import { faMobile } from '@fortawesome/free-solid-svg-icons'
-import { faBook } from '@fortawesome/free-solid-svg-icons'
-import { faWineBottle } from '@fortawesome/free-solid-svg-icons'
-// import { fausb } from '@fortawesome/free-solid-svg-icons'
-import { fasPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faUsb
+    
+} from '@fortawesome/free-brands-svg-icons'
+import { faInfoCircle, 
+    faMobile,
+    faBook,
+    faTintSlash,
+    faPencilAlt
+} from '@fortawesome/free-solid-svg-icons'
 
 
 class AlertsPopUp extends Component {
@@ -29,22 +33,22 @@ class AlertsPopUp extends Component {
 
                     </div>
                     <div style ={styles.container} >
-                        <FontAwesomeIcon icon={faBook} size="3x" color="#8F2A92" />
+                        <FontAwesomeIcon icon={faBook} size="2x" color="#8F2A92" />
                         <p>Libros o cuadernos</p>
 
                     </div>
                     <div style ={styles.container} >
-                        <FontAwesomeIcon icon={faWineBottle} size="3x" color="#8F2A92"/>
+                        <FontAwesomeIcon icon={faTintSlash} size="2x" color="#8F2A92"/>
                         <p>Envases no transparentes</p>
 
                     </div>
                     <div style ={styles.container} >
-                    {/* <FontAwesomeIcon icon={fausb} size="3x" color="#8F2A92"/> */}
+                    <FontAwesomeIcon icon={faUsb} size="2x" color="#8F2A92"/>
                         <p>USBs</p>
 
                     </div>
                     <div style ={styles.container} >
-                    {/* <FontAwesomeIcon icon={ fasPencilAlt } size="3x" color="#8F2A92"/> */}
+                    <FontAwesomeIcon icon={ faPencilAlt } size="2x" color="#8F2A92"/>
                         <p>Lapicero </p>
 
                     </div>
@@ -59,8 +63,6 @@ class AlertsPopUp extends Component {
     }
 }
 
-// <i class="fas fa-pencil-alt"></i>
-
 const styles = {
     header : 
     {
@@ -70,6 +72,7 @@ const styles = {
     body : 
     {
         display: 'flex',
+        justifyContent: 'center',
         flexDirection : 'column',
         width : '30%',
         height : '100vh',
@@ -78,11 +81,9 @@ const styles = {
     container : 
     {
         display: 'flex',
-        // justifyContent: 'space-Evenly',
         width: '100%',
         alignItems : 'center',       
-        // textDecoration: 'underline',
-        margin: '30px 0 0 54px',
+        margin: '30px 0 0 50px',
     },
 
   }
