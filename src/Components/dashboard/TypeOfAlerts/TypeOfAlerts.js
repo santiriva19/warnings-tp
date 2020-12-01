ed56e6c7c8cbe968103a51e116fc6af540c263f2
santiriva19 <santiriva19@gmail.com>
2020-12-01 08:44:53 -0500

    algo

--- src/Components/dashboard/TypeOfAlerts/TypeOfAlerts.js
+++ src/Components/dashboard/TypeOfAlerts/TypeOfAlerts.js
@@ -14,95 +14,56 @@ import { faInfoCircle,
     faPencilAlt
 } from '@fortawesome/free-solid-svg-icons'
 
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
 
-class AlertsPopUp extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            showMenu: false
-        }
+export default props => {
+  return (
+    <Menu>
+        <div style ={styles.body}> 
+            <div style ={styles.header}>
+                <FontAwesomeIcon 
+                    icon = {faInfoCircle} size="4x" color="#8F2A92"
+                    style = {{width : '100%'}}
+                />
+                <h2 style={{color: '#575757', textAlign: 'center'}}>Tipo de alertas...</h2>
+            </div>        
+            <div style ={styles.container}>
+                <FontAwesomeIcon icon={faMobile} size="3x" color="#8F2A92" />
+                <p >Smat Phones</p>
 
-        this.menuShowToggle = this.menuShowToggle.bind(this);
-    }
-
-    menuShowToggle = () => {
-        this.setState({showMenu: !this.state.showMenu})
-    }
-
-    render ( ) {
-        return (
-            <div >
-                <div style ={styles.body}> 
-                    <div style ={styles.header}>
-                        <FontAwesomeIcon 
-                            icon = {faInfoCircle} size="4x" color="#8F2A92"
-                            style = {{width : '100%'}}
-                        />
-                        <h2 style={{color: '#575757', textAlign: 'center'}}>Tipo de alertas...</h2>
-                    </div>        
-                    <div style ={styles.container}>
-                        <FontAwesomeIcon icon={faMobile} size="3x" color="#8F2A92" />
-                        <p >Smat Phones</p>
-
-                    </div>
-                    <div style ={styles.container} >
-                        <FontAwesomeIcon icon={faBook} size="2x" color="#8F2A92" />
-                        <p>Libros o cuadernos</p>
-
-                    </div>
-                    <div style ={styles.container} >
-                        <FontAwesomeIcon icon={faTintSlash} size="2x" color="#8F2A92"/>
-                        <p>Envases no transparentes</p>
-
-                    </div>
-                    <div style ={styles.container} >
-                    <FontAwesomeIcon icon={faUsb} size="2x" color="#8F2A92"/>
-                        <p>USBs</p>
-
-                    </div>
-                    <div style ={styles.container} >
-                    <FontAwesomeIcon icon={ faPencilAlt } size="2x" color="#8F2A92"/>
-                        <p>Lapicero </p>
-
-                    </div>
-                    <div style = {{display: 'flex', justifyContent: 'center'}}>
-                        <button 
-                            style = {styles.button}
-                            type = "button"
-                            onClick = { this.menuShowToggle }
-                            value = "toggle"  
-                        > Salir
-                            {/*change displey from flex to none by DOM  */} 
-                        </button>
-                    </div>
-                </div>
             </div>
-        )
-    }
-}
+            <div style ={styles.container} >
+                <FontAwesomeIcon icon={faBook} size="2x" color="#8F2A92" />
+                <p>Libros o cuadernos</p>
 
-const styles = {
-    header : 
-    {
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    body : 
-    {
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection : 'column',
-        width : '30%',
-        height : '100vh',
-    },
-    container : 
-    {
-        display: 'flex',
-        width: '100%',
-        alignItems : 'center',       
-        margin: '30px 0 0 50px',
-    },
+            </div>
+            <div style ={styles.container} >
+                <FontAwesomeIcon icon={faTintSlash} size="2x" color="#8F2A92"/>
+                <p>Envases no transparentes</p>
 
-  }
+            </div>
+            <div style ={styles.container} >
+            <FontAwesomeIcon icon={faUsb} size="2x" color="#8F2A92"/>
+                <p>USBs</p>
 
-export default AlertsPopUp;
+            </div>
+            <div style ={styles.container} >
+            <FontAwesomeIcon icon={ faPencilAlt } size="2x" color="#8F2A92"/>
+                <p>Lapicero </p>
+
+            </div>
+            <div style = {{display: 'flex', justifyContent: 'center'}}>
+                <button 
+                    style = {styles.button}
+                    type = "button"
+                    onClick = { this.menuShowToggle }
+                    value = "toggle"  
+                > Salir
+                    {/*change displey from flex to none by DOM  */} 
+                </button>
+            </div>
+        </div>
+    </Menu>
+  );
+};
